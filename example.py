439317e098e58e442464bb834178d1548a8d76cc
wr247 <wr24753_mika@outlook.jp>
2025-09-01 14:26:30 +0900

    自己紹介をするプログラムの課題完了

--- example.py
+++ example.py
@@ -1,1 +1,79 @@
-print("Hello, world!")
+print("Hello, world!") 
+print(100)
+print(3.1415)
+print('3.14')
+print("テックアカデミー")
+print(50 + 3 + 2)
+print(3.14159 - 3)
+print("テック" + "アカデミー")
+print(3+テックアカデミー)
+print(3+統計学)
+print("テック" - "アカデミー")
+print(3+"統計学")
+3+2
+type(3)
+type("3.24259")
+type(3.14159)
+type(1.)
+type(-1)
+1e+5
+1e-4
+10000 + 300 - 2000
+8*4
+54/6
+21/8
+21//8
+21%8
+8**4
+2^6
+3 * 4.6
+0.1 + 0.1 + 0.1
+round(3*4.6,1)
+int(123.456)
+froat(123.456)
+float(123.456)
+int(0.999)
+float(56789)
+int("1234567890")
+float("3.14159265358")
+float("9414513")
+int("3.0")
+int(float("3.0"))
+int("一九九九")
+int("1,999,000")
+print("Hello, "Python" world!")
+print("hello","python""world")
+print('I'm Taro Tech')
+print('Hello, "Python" world!')
+print("Hello, "Python" world!")
+print("Hello, 'python' world")
+print('Hello, "Python" world! I'm Taro Tech!')
+print('hello, \'pyhon\' world')
+print("""hello,"python" world""")
+print('''I'm Taro Tech.''')
+print("""統計学
+---
+統計学（とうけいがく、英：statistics）とは、統計に関する研究を行う学問である。""")
+print("""統計学
+---
+統計学(とうけいがく、英: statistics)とは、統計に関する研究を行う学問である。""")
+print("""統計学
+      ---
+      統計学(とうけいがく、英:statistics)とは、統計に関する研究を行う学問である。""")
+print("Hello,\nPython\nworld!")
+print("Hello,
+Python
+world!")
+print("Hello,\nPython\nworld!")
+type(str(123))
+len(str(12345))
+a=2
+b=998
+print(a)
+a = 2
+b = 998
+print(a)
+a=2
+b=998
+print(a)
+print(b)
---
+++ m-kikuchi.py
@@ -0,0 +1,44 @@
+a = 2
+b = 998
+print(a)
+a = 2; b = 998; print(a)
+print(a)
+c=30; print(c)
+print(b)
+a+b+c
+def hello():
+    print("Hello!")
+    print("It is a rainy day today!")
+
+def good():
+    print("Good morning!")
+    print("Have a nice day!"); good()
+
+def hello():
+    print("Hello!")
+    print("It is a fine day today!")
+
+def good_morning():
+    print("Good morning!")
+    print("Have a nice day!")
+
+hello()
+good_morning()
+a=2
+b=998
+print(a)
+def good_morning():
+    print("Good morning!")
+    print("Have a nice day!")
+good_morning()
+def hello():
+    print("Hello!")
+    print("It is a fine day today!")
+hello()
+
+name="bas"
+def calc_bmi(height, weight):
+    ret = weight / (height ** 2)
+    return ret
+height = float(input("身長(m)を入力："))
+weight = float(input("体重(kg)を入力："))
